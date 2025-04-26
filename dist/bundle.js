60b8cd7ca5ac7b6a5182416ec5d4c65289bbabbb
Logit <mrnoob500@gmail.com>
2025-04-26 08:51:59 -0700

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -4589,7 +4589,7 @@ window.prices;
 
 async function fetchPrices() {
     try {
-        const response = await fetch('https://ghproxy.net/https://raw.githubusercontent.com/holychikenz/MWIApi/refs/heads/main/milkyapi.json'
+        const response = await fetch('https://raw.githubusercontent.com/holychikenz/MWIApi/refs/heads/main/milkyapi.json'
             , {
                 mode: 'cors'
             }
